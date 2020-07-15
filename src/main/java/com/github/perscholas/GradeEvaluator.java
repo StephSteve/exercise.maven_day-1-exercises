cf67483616fa30f2284243509f993b9d981d5a1d
StephSteve <stephanie.oncallbusiness@gmail.com>
2020-07-15 10:33:57 -0400

    Grade evaluator

--- src/main/java/com/github/perscholas/GradeEvaluator.java
+++ src/main/java/com/github/perscholas/GradeEvaluator.java
@@ -1,30 +1,46 @@
 package com.github.perscholas;
 
+import java.sql.Struct;
+
 /**
  * Created by leon on 7/14/2020.
  */
 public class GradeEvaluator {
     public Boolean isGradeA(Double score) {
-        return null;
+        if (score <= 100 && score >=90){
+        }return true;
     }
 
     public Boolean isGradeB(Double score) {
-        return null;
-    }
+        return (score <= 89 && score >=80);
 
     public Boolean isGradeC(Double score) {
-        return null;
+            return (score <= 79 && score >=70);
     }
 
     public Boolean isGradeD(Double score) {
-        return null;
+            return (score <= 79 && score >=70);
     }
 
     public Boolean isGradeF(Double score) {
-        return null;
+            return score <= 64;
     }
 
     public Character getGrade(Double score) {
-        return null;
+       if (isGradeA(score)){
+           return 'A';
+       }
+            if (isGradeB(score)){
+                return 'B';
+            }
+            else if (isGradeC(score)){
+                return 'C';
+            }
+            if (isGradeD(score)){
+                return 'D';
+            }
+            if (isGradeF(score)){
+                return 'F';
+            }
     }
 }
